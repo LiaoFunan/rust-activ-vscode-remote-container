d930dd4ab3c073b33f15bb5f73c8d7aedb79e79e
Johnny.Liao <liao.funan@gmail.com>
2021-04-14 10:05:19 +0800

    add json vec response

--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,70 @@
 use actix_web::{get, web, HttpResponse, Result};
 use serde::{Deserialize, Serialize};
 
+// #[macro_use]
+// extern crate derive_new;
+
 #[derive(Serialize, Deserialize)]
 struct MyObj {
     name: String,
 }
 
+impl MyObj {
+    fn new(name_value: String) -> MyObj {
+        MyObj { name: name_value }
+    }
+}
+
+// #[derive(new)]
+// struct Book {
+//     title: String,
+//     author: String,
+//     genres: String,
+//     pages: u32,
+// }
+
+
+// #[test]
+// fn test_book() {
+//     let common_book = Book::new(
+//         String::from("Common"),
+//         String::new(),
+//         String::from("Programming"),
+//         100,
+//     );
+//     assert_eq!(
+//         common_book,
+//         Book {
+//             title: String::from("Common"),
+//             author: String::new(),
+//             genres: "Programming".to_string(),
+//             pages: 100
+//         }
+//     );
+
+//     let another_book = Book::new(String::from("Hey"), String::from("abc"), "Fiction".to_string(), 150);
+//     let zz = MyObj::new(String::from("Hey"));
+// }
+
+
 #[get("/a/{name}")]
-async fn index(obj: web::Path<MyObj>) -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok().json(MyObj {
-        name: obj.name.to_string(),
-    }))
+async fn index(_obj: web::Path<MyObj>) -> Result<HttpResponse> {
+
+    // let a = MyObj {
+    //     name: obj.name.to_string(),
+    // };
+
+    let mut v: Vec<MyObj> = Vec::new();
+
+    let qq = MyObj::new(String::from("Hey"));
+    let pp = MyObj::new(String::from("xxxx"));
+    v.push(qq);
+    v.push(pp);
+
+    //  let a = MyObj {
+    //     name: obj.name.to_string(),
+    // };
+    Ok(HttpResponse::Ok().json(v))
 }
 
 #[actix_web::main]
